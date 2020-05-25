7aeaa681d656d7861c8fa7037b7a3931769abdc3
zzo2741 <dlwnstmd@gmail.com>
2020-05-25 20:56:43 +0900

    0525

--- IO.py
+++ IO.py
@@ -1,4 +1,73 @@
+# 고양이
 print("\    /\ ")
 print(" )  ( ') ")
 print("(  /  ) ")
 print(" \(__)| ")
+print()
+# 개
+print("|\_/| ")
+print("|q p|   /} ")
+print('( 0 )"""\ ')
+print('|"^"`    | ')
+print("||_/=\\\__| ")
+print()
+# A+B
+numStr = "1 2"
+num = numStr.split(" ")
+print(int(num[0]) + int(num[1]))
+print()
+# A-B
+numStr = "3 2"
+num = numStr.split(" ")
+print(int(num[0]) - int(num[1]))
+print()
+# A*B
+numStr = "3 2"
+num = numStr.split(" ")
+print(int(num[0]) * int(num[1]))
+print()
+# A/B
+numStr = "1 3"
+num = numStr.split(" ")
+print(int(num[0]) / int(num[1]))
+print()
+# 사칙연산
+numStr = "7 3"
+num = numStr.split(" ")
+print(int(num[0]) + int(num[1]))
+print(int(num[0]) - int(num[1]))
+print(int(num[0]) * int(num[1]))
+print(int(int(num[0]) / int(num[1])))
+print(int(num[0]) % int(num[1]))
+print()
+# 나머지
+numStr = "5 8 4"
+num = numStr.split(" ")
+A = int(num[0])
+B = int(num[1])
+C = int(num[2])
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print( ((A%C)*(B%C))%C)
+print()
+# 곱셈
+
+# numStr1 = "472"
+# numStr2 = "385"
+numStr1 = input()
+numStr2 = input()
+num1 =  int(numStr1)
+i = 2
+gob = 1
+result = 0
+while i>=0:
+    gobNum1 = (num1 * int(numStr2[i])) * gob
+    gobNum2 = (num1 * int(numStr2[i]))
+    i = i - 1
+    gob = gob * 10
+    result = result + gobNum1
+    print(gobNum2)
+print(result)
+
+
--- if.py
+++ if.py
@@ -1,11 +1,54 @@
-num = input()
+# 두 수 비교하기
+# num = input()
+num = "1 2"
 num1 = num.split(" ")
 A = int(num1[0])
 B = int(num1[1])
-
 if(A > B):
     print(">")
 elif(A < B):
     print("<")
 else:
     print("==")
+print()
+# 시험 성적
+score = 100
+# score = int(input())
+if(90 <= score and score <= 100):
+    print("A")
+elif(80 <= score and score <= 89):
+    print("B")
+elif(70 <= score and score <= 79):
+    print("C")
+elif(60 <= score and score <= 69):
+    print("D")
+else:
+    print("F")
+print()
+# 윤년
+leapYear = 2000
+# leapYear = int(input())
+answer = 0
+if(leapYear % 4 == 0):
+    if(leapYear % 100 != 0 or leapYear % 400 == 0):
+        answer = 1;
+print(answer)
+
+# 사분면
+# x = int(input())
+# y = int(input())
+x = 9
+y = -13
+if(0 < x):
+    if(0 < y):
+        print(1)
+    else:
+        print(4)
+else:
+    if (0 < y):
+        print(2)
+    else:
+        print(3)
+
+
+
